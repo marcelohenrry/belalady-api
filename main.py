0b4dc0a01a830b011d3d723d6e8a9356509604f7
DevCoderMax <gleysondonascimentocarvalho@gmail.com>
2026-01-13 10:06:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ logger = logging.getLogger(__name__)
 origins = [
     "http://localhost:3000",
     "https://belalady-front-dqzi-git-verce-4dc358-gleysons-projects-e8508ef4.vercel.app",
-    "https://belalady-front-git-vercel-rea-8d8046-gleysons-projects-e8508ef4.vercel.app"
+    "https://belalady-front-git-vercel-rea-8d8046-gleysons-projects-e8508ef4.vercel.app",
+    "https://max-belalady-web-marcelo.uvxtdw.easypanel.host"
 ]
 app.add_middleware(CORSMiddleware,
                    allow_origins=origins,
